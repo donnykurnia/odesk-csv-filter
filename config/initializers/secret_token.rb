8197075991d656d8c240136a380faf21a5cf6741
Donny Kurnia <donnykurnia@gmail.com>
2013-10-05 09:06:51 +0700

    Rails 4.0 with postgresql
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OdeskCsvFilter::Application.config.secret_key_base = '0f18797b2469c2d472fcf342267c382daf9b426d5eb0584bca9472f129e3e79cc7702e03366b3df44609187a9fd19b83544760f14a322305578829c2226148ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OdeskCsvFilter::Application.config.session_store :cookie_store, key: '_odesk-csv-filter_session'
