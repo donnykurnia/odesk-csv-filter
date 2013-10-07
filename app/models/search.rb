b48e99c62c7d93f2721626053f92e42e81e98793
Donny Kurnia <donnykurnia@gmail.com>
2013-10-07 19:13:00 +0700

    add search model
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

---
+++ app/models/search.rb
@@ -0,0 +1,14 @@
+class Search < OpenStruct
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend  ActiveModel::Naming
+
+  def initialize(*args)
+    super
+  end
+
+  def persisted?
+    false
+  end
+
+end
