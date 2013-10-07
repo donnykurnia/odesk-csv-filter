c0ec37187d71debe2fa422c9c30743317dec1243
Donny Kurnia <donnykurnia@gmail.com>
2013-10-07 18:47:49 +0700

    bulk insert

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,6 +13,12 @@ class Product < ActiveRecord::Base
   end
 
   def self.bulk_insert(products_params)
+    products_params[:kind].each_with_index do |kind, index|
+      self.create(:kind => kind.strip,
+                  :height => products_params[:height][index].strip,
+                  :is_emergency_exit => products_params[:is_emergency_exit][index].strip,
+                  :is_openable => products_params[:is_openable][index].strip)
+    end
   end
 
 end
