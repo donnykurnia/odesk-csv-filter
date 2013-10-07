e98fc391c290bf5d148731bc8035ae6b0eef4cef
Donny Kurnia <donnykurnia@gmail.com>
2013-10-07 17:10:17 +0700

    add validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,18 @@
 class Product < ActiveRecord::Base
 
+  validates :kind, presence: true
+  validates :height, presence: true, numericality: { only_integer: true }
+
   # methods
-  def csv_headers
-    ["Kind", "Height", "Emergency exit", "Openable"]
-  end
 
   # class methods
   def self.search(search_params)
     products = all
     # products = products.where("products.title like :search OR products.description like :search", { search: "%" + search_params + "%" }) unless search_params.blank?
     products
+  end
+
+  def self.bulk_insert(products_params)
   end
 
 end
