b1e96690ec4acbcc6ab403e179b8c566b7677f49
Donny Kurnia <donnykurnia@gmail.com>
2013-10-11 07:10:14 +0700

    add accepts_nested_attributes_for

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,6 +3,7 @@ class Search < ActiveRecord::Base
   has_many :connections, class_name: "Search",
                           foreign_key: "parent_id",
                           dependent: :destroy
+  accepts_nested_attributes_for :connections, allow_destroy: true
   belongs_to :parent, class_name: "Search"
   has_many :search_results
 end
